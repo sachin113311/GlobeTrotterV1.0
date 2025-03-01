ac13ccbf2d3ad3b4d20c98de9c24ebbc561882f6
sachin113311 <ss151245@gmail.com>
2025-03-01 22:13:19 +0530

    Update city-trivia.service.ts

--- src/app/city-trivia.service.ts
+++ src/app/city-trivia.service.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
   providedIn: 'root'
 })
 export class CityTriviaService {
-  private apiUrl = "http://localhost:5000/api";
+  private apiUrl = "https://globetrotterv1-0-server.onrender.com/api";
 
   async registerUser(username: string) {
     const response = await axios.post(`${this.apiUrl}/users/register`, { username });
